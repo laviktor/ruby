7309b5c28bafc73e2642e001e42a8be783f6a3e0
V. Kato La <56576057+laviktor@users.noreply.github.com>
2022-04-27 14:03:25 -0400

    Update ruby_in_20.rb

--- ruby_in_20.rb
+++ ruby_in_20.rb
@@ -1,18 +1,18 @@
 puts "Hello World"
 Math.sqrt(9)
 def hi
-puts "Hello World!"
+  puts "Hello World!"
 end
 hi
 hi() # It turns out calling the method (equivalent to function?) is sufficient. Parenthesis optional.
 # Now taking an input. Note the method does not need to be renamed, it can be redefined.
 def hi(name)
-puts "Hello #{name}!"
+  puts "Hello #{name}!"
 end
 hi("Kato")
 # Now defaulting if there is no input, and no parenthesis, even if calling a method with an input
 def hi(name = "World")
-puts "Hello #{name.capitalize}!"
+  puts "Hello #{name.capitalize}!"
 end
 hi "kato"
 hi
